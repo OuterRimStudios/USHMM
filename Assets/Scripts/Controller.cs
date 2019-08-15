d8e8e581b34f6255ca280a6e62308d8aeb747cca
hectorborges <developer@hectorborges.com>
2019-08-15 16:33:08 -0500

    Initial commit

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Handedness { Left, Right }
+
+public class Controller : MonoBehaviour
+{
+    public Handedness handedness;
+
+    public float interactionRadius = .5f;
+    public LayerMask interactionLayer;
+
+    InputManager inputManager;
+
+    Transform heldObject;
+
+    void Start()
+    {
+        inputManager = InputManager.Instance;
+    }
+
+    void Update()
+    {
+        transform.position = handedness == Handedness.Left ? inputManager.LeftPosition : inputManager.RightPosition;
+        transform.rotation = handedness == Handedness.Left ? inputManager.LeftRotation : inputManager.RightRotation;
+
+        Collider[] interactables = Physics.OverlapSphere(transform.position, interactionRadius, interactionLayer);
+
+        float grip = (handedness == Handedness.Left ? inputManager.LeftGrip : inputManager.RightGrip);
+
+        if (interactables.Length > 0 && !heldObject && grip > 0)
+        {
+            heldObject = interactables[0].transform;
+
+            heldObject.transform.position = transform.position;
+            heldObject.SetParent(transform);
+        }
+
+        if(heldObject && grip <= 0)
+        {
+            heldObject.SetParent(null);
+            heldObject = null;
+        }
+    }
+}
+
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance;
+
+    Controls controls;
+
+    public float LeftGrip { get; private set; }
+    public float RightGrip { get; private set; }
+
+    public Vector3 LeftPosition { get; private set; }
+    public Vector3 RightPosition { get; private set; }
+
+    public Quaternion LeftRotation { get; private set; }
+    public Quaternion RightRotation { get; private set; }
+
+    void Awake()
+    {
+        Instance = this;
+        controls = new Controls();
+        controls.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    void Update()
+    {
+        GetInput();
+    }
+
+    void GetInput()
+    {
+        LeftGrip = controls.Input.LeftGrip.ReadValue<float>();
+        RightGrip = controls.Input.RightGrip.ReadValue<float>();
+
+        LeftPosition = controls.Input.LeftPosition.ReadValue<Vector3>();
+        RightPosition = controls.Input.RightPosition.ReadValue<Vector3>();
+
+        LeftRotation = controls.Input.LeftRotation.ReadValue<Quaternion>();
+        RightRotation = controls.Input.RightRotation.ReadValue<Quaternion>();
+    }
+}

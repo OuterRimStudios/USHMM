c782e68cca61ed0bbc8374f367b628aa29161e19
hectorborges <developer@hectorborges.com>
2019-08-19 17:05:59 -0500

    Added timeline for alley

---
+++ Assets/Scripts/Event.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event : MonoBehaviour
+{
+    public void StartEvent()
+    {
+        print("Event Started");
+    }
+
+    public void StopEvent()
+    {
+        print("Event Ended");
+    }
+}

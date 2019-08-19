048dc0993a07f639d3b54fb71999ce4c57f8dc4f
hectorborges <developer@hectorborges.com>
2019-08-19 12:13:40 -0500

    Added base interaction

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OuterRimStudios.Utilities;
+
+public class Interactable : MonoBehaviour
+{
+    public bool useProximity;
+    public float proximityDistance;
+
+    public bool InProximity { get; set; }
+    Transform player;
+
+    public virtual void Start()
+    {
+        if(useProximity)
+            player = GameObject.Find("Player").transform;
+    }
+
+    public virtual void Interact() { }
+
+    public virtual void Update()
+    {
+        if (!useProximity) return;
+        Proximity();
+        print(InProximity);
+    }
+
+    void Proximity()
+    {
+        InProximity = MathUtilities.CheckDistance(player.position, transform.position) < proximityDistance;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(useProximity)
+            Gizmos.DrawWireSphere(transform.position, proximityDistance);
+    }
+}

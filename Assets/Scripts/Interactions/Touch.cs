230785596ef7474cba403648d9f7809b99a1f6a8
hectorborges <developer@hectorborges.com>
2019-08-19 12:57:14 -0500

    Added Touch interaction

---
+++ Assets/Scripts/Interactions/Touch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touch : Interactable
+{
+    public override void Interact()
+    {
+        base.Interact();
+        print("Interact");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag.Equals("Controller"))
+        {
+            if(!Triggered)
+                Interact();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag.Equals("Controller"))
+        {
+            if(Triggered && isRepeatable)
+                Reset();
+        }
+    }
+}

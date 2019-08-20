abe87d9aaf830ed6c2af0b4b2eedd3bf00ab45e5
hectorborges <developer@hectorborges.com>
2019-08-20 13:23:51 -0500

    testing intro

---
+++ Assets/Scripts/TimelineEvents/Intro Events/DocumentsEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DocumentsEvent : Event
+{
+    public Animator documentAnimator;
+    public override void StartEvent()
+    {
+    }
+
+    public override void StopEvent()
+    {
+        base.StopEvent();
+    }
+}
---
+++ Assets/Scripts/Utilities/Grow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grow : MonoBehaviour
+{
+    public float scaleSpeed;
+    public Vector3 targetSize;
+
+    public IEnumerator Start()
+    {
+
+        yield return new WaitForSeconds(.5f);
+        StartCoroutine(Scale());
+    }
+
+    IEnumerator Scale()
+    {
+        yield return new WaitUntil(() => IsScaled());
+    }
+
+    bool IsScaled()
+    {
+        transform.localScale = Vector3.MoveTowards(transform.localScale, targetSize, scaleSpeed * Time.deltaTime);
+        return transform.localScale == targetSize;
+    }
+}

a33b774927064b8a9fdac3b00a93b173d573138a
hectorborges <developer@hectorborges.com>
2019-08-21 15:25:34 -0500

    added scene fading

---
+++ Assets/Scripts/TimelineEvents/FadeEvent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public enum FadeType { FadeIn, FadeOut }
+
+public class FadeEvent : Event
+{
+    public Image fadePanel;
+    public float fadeSpeed = 1;
+
+    public bool loadScene;
+    public string sceneName;
+    Color color;
+
+    public void StartEvent(bool fadeIn)
+    {
+        color.a = fadeIn == true ? 0 : 1;
+        StartCoroutine(Fade(fadeIn == true ? 1 : 0));
+    }
+
+    IEnumerator Fade(int value)
+    {
+        yield return new WaitUntil(() => Faded(value));
+
+        if (loadScene)
+            SceneManager.LoadScene(sceneName);
+    }
+
+    bool Faded(int value)
+    {
+        color.a = Mathf.MoveTowards(color.a, value, fadeSpeed * Time.deltaTime);
+        fadePanel.color = color;
+        return fadePanel.color.a == value;
+    }
+}

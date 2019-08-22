b0f6c0c4cf6c175f399e82dc115ef01ffd9109a8
hectorborges <38707692+hectorborges@users.noreply.github.com>
2019-08-22 15:01:39 -0500

    add some optimization

---
+++ Assets/Scripts/ResolutionScale.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class ResolutionScale : MonoBehaviour
+{
+    public float resolutionScale = 1;
+
+    void Start()
+    {
+        XRSettings.eyeTextureResolutionScale = resolutionScale;
+    }
+}

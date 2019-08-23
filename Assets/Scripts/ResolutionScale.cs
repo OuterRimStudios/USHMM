2c583d4ce8b9bbbfba04c580403c57ca02665421
hectorborges <38707692+hectorborges@users.noreply.github.com>
2019-08-23 11:06:23 -0500

    removed fog and enviro from scene

--- Assets/Scripts/ResolutionScale.cs
+++ Assets/Scripts/ResolutionScale.cs
@@ -9,6 +9,7 @@ public class ResolutionScale : MonoBehaviour
 
     void Start()
     {
+        Application.targetFrameRate = 90;
         XRSettings.eyeTextureResolutionScale = resolutionScale;
     }
 }

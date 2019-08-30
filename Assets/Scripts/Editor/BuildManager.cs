12a5efbacb781bddc82d090baf86fdfa36eeb81e
Sam Gebhard <33882552+sgebhard6@users.noreply.github.com>
2019-08-30 12:52:02 -0500

    added script to build scenes to separate folders, added script to load different scenes/executables

---
+++ Assets/Scripts/Editor/BuildManager.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildManager
+{
+    [MenuItem("Build Tools/Build")]
+    public static void BuildProject()
+    {
+        //Get filename
+        string path = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
+        List<string> levels = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+            levels.Add(scene.path);
+
+        //Build
+        List<string> exePaths = new List<string>();
+        foreach (string level in levels)
+        {
+            string folderName = level.Substring(13);
+            string exePath = path + folderName + "/Build.exe";
+            exePaths.Add(exePath);
+            BuildPipeline.BuildPlayer(new string[] { level }, exePath, BuildTarget.StandaloneWindows64, BuildOptions.Development);
+        }
+
+        SceneManifest.CreateSceneManifest(exePaths, path);
+    }
+
+    [MenuItem("Build Tools/Build & Run")]
+    public static void BuildRunProject()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Utilities/XMLOp.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+public class XMLOp {
+
+    /// <summary> Saves data from an item to a file</summary>
+    /// <param name="item"> object to write</param>
+    /// <param name="path">filename</param>
+	public static void Serialize(object item, string path)
+    {
+        XmlSerializer serializer = new XmlSerializer(item.GetType());
+        StreamWriter writer = new StreamWriter(path);
+        serializer.Serialize(writer.BaseStream, item);
+        writer.Close();
+    }
+
+    /// <summary>Reads data from a file</summary>
+    /// <typeparam name="T">Type of object that is stored</typeparam>
+    /// <param name="path">filename</param>
+    /// <returns>Object of specified type with data read from file</returns>
+    public static T Deserialize<T> (string path)
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(T));
+        StreamReader reader = new StreamReader(path);
+        T deserialized = (T)serializer.Deserialize(reader.BaseStream);
+        reader.Close();
+        return deserialized;
+    }
+}

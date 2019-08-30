3d3b6807811cfe09857051e6ce9b47342280e335
Sam Gebhard <33882552+sgebhard6@users.noreply.github.com>
2019-08-30 14:27:56 -0500

    added timelinecontroller script

---
+++ Assets/Scripts/Utilities/TimelineController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineController : MonoBehaviour
+{
+    public PlayableDirector director;
+    public KeyCode togglePauseKey = KeyCode.Space;
+    /*
+    [Space, Header("Timestepping")]
+    public KeyCode restartKey = KeyCode.Home;
+    public KeyCode incrementTimeKey = KeyCode.RightArrow;
+    public KeyCode decrementTimeKey = KeyCode.LeftArrow;
+    public float timeStep = 10;
+    */
+
+    void Update()
+    {
+        if (Input.GetKeyDown(togglePauseKey))
+        {
+            if (director.state == PlayState.Paused)
+                director.Resume();
+            else
+                director.Pause();
+        }
+
+        /*
+        if (Input.GetKeyDown(restartKey))
+            director.time = 0;
+
+        if (Input.GetKeyDown(incrementTimeKey))
+        {
+            if (director.time + timeStep > director.duration)
+                director.time = director.duration;
+            else
+                director.time += timeStep;
+        }
+
+        if (Input.GetKeyDown(decrementTimeKey))
+        {
+            if (director.time - timeStep < 0)
+                director.time = 0;
+            else
+                director.time -= timeStep;
+        }
+        */
+    }
+}

ceb29873d8d902f5e7da7f9f19fec22a25f944d1
hectorborges <38707692+hectorborges@users.noreply.github.com>
2019-08-30 16:50:41 -0500

    quick changes

--- Assets/Scripts/ResolutionScale.cs
+++ Assets/Scripts/ResolutionScale.cs
@@ -5,13 +5,17 @@ using UnityEngine.XR;
 
 public class ResolutionScale : MonoBehaviour
 {
+    public bool enableInEditor;
     public float resolutionScale = 1;
 
     void Start()
     {
         Application.targetFrameRate = 90;
 #if UNITY_EDITOR
-        XRSettings.eyeTextureResolutionScale = 1;
+        if (!enableInEditor)
+            XRSettings.eyeTextureResolutionScale = 1;
+        else
+            XRSettings.eyeTextureResolutionScale = resolutionScale;
 #elif UNITY_STANDALONE
         XRSettings.eyeTextureResolutionScale = resolutionScale;
 #endif

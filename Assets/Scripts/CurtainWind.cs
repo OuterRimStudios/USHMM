a7934e24249ced190f4868c07d2bfbd00e4ce86a
Sam Gebhard <33882552+sgebhard6@users.noreply.github.com>
2019-09-03 15:18:42 -0500

    added random rotation to CurtainWind, tied curtain wind gust to timeline, added audio stuff to curtainwind

--- Assets/Scripts/CurtainWind.cs
+++ Assets/Scripts/CurtainWind.cs
@@ -7,24 +7,42 @@ public class CurtainWind : MonoBehaviour
 {
     public Cloth curtain;
     public CapsuleCollider[] colliders;
+    public AudioSource source;
     public float minSpeed;
     public float maxSpeed;
     public float moveDist;
     float speed;
     Vector3 targetPos;
 
+    void OnEnable()
+    {
+        CurtainEvent.OnGust += StartGust;
+    }
+
+    void OnDisable()
+    {
+        CurtainEvent.OnGust -= StartGust;
+    }
+
     private void Start()
     {
         curtain.capsuleColliders = colliders;
+        foreach (CapsuleCollider collider in colliders)
+            collider.transform.rotation = Random.rotation;
         speed = Random.Range(minSpeed, maxSpeed);
+    }
+
+    void StartGust()
+    {
         StartCoroutine(Gust());
     }
 
     IEnumerator Gust()
     {
         targetPos = transform.position + (transform.forward * moveDist);
-        yield return new WaitForSeconds(2f);
+        source.Play();
         yield return new WaitUntil(MoveColliders);
+        source.Stop();
     }
 
     bool MoveColliders()

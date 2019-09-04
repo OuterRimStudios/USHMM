6c336ba3eaff472bc949d4d91b34ee14f0772558
hectorborges <38707692+hectorborges@users.noreply.github.com>
2019-09-04 13:27:33 -0500

    added skybox rotation

---
+++ Assets/Scripts/Utilities/RotateSkybox.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSkybox : MonoBehaviour
+{
+    public float speed;
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * speed);
+    }
+}

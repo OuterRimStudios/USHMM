2590c6c91ce63e6775b5b79aae5dca67c05d270b
hectorborges <developer@hectorborges.com>
2019-09-10 16:24:14 -0500

    added survey code

---
+++ Assets/Answer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Answer : MonoBehaviour
+{
+    public Color selectedColor;
+
+    Image image;
+    Color originalColor;
+    public TextMeshProUGUI text { get; private set; }
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+        originalColor = image.color;
+        text = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void Selected()
+    {
+        image.color = selectedColor;
+    }
+
+    public void DeSelected()
+    {
+        image.color = originalColor;
+    }
+}
---
+++ Assets/SurveyManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurveyManager : MonoBehaviour
+{
+    public List<QuestionField> questions;
+    public GameObject next;
+    public GameObject submit;
+
+    int currentQuestion = 0;
+
+    public void Next()
+    {
+        questions[currentQuestion].gameObject.SetActive(false);
+
+        if (currentQuestion >= (questions.Count - 1))
+            currentQuestion = 0;
+        else
+            currentQuestion++;
+
+        questions[currentQuestion].gameObject.SetActive(true);
+
+        if (currentQuestion == questions.Count - 1)
+        {
+            next.SetActive(false);
+            submit.SetActive(true);
+        }
+        else if(!next.activeInHierarchy)
+        {
+            submit.SetActive(false);
+            next.SetActive(true);
+        }
+    }
+
+    public void Previous()
+    {
+        questions[currentQuestion].gameObject.SetActive(false);
+
+        if (currentQuestion == 0)
+            currentQuestion = questions.Count - 1;
+        else
+            currentQuestion--;
+        
+        questions[currentQuestion].gameObject.SetActive(true);
+
+        if (currentQuestion == questions.Count - 1)
+        {
+            next.SetActive(false);
+            submit.SetActive(true);
+        }
+        else if (!next.activeInHierarchy)
+        {
+            submit.SetActive(false);
+            next.SetActive(true);
+        }
+    }
+
+    public void Submit()
+    {
+        List<string> answers = new List<string>();
+
+        foreach (QuestionField question in questions)
+            answers.Add(question.answer);
+
+        Debug.Log("Survey Submitted");
+        gameObject.SetActive(false);
+        //Send to analytics
+    }
+}

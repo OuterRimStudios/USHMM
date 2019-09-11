4cb023851379e0e77328afeb3869d3b0b7d0ce5b
hectorborges <developer@hectorborges.com>
2019-09-11 08:48:43 -0500

    testing VR interactions

---
+++ Assets/Scripts/Interact/Interactions/UnityEventInteraction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UnityEventInteraction : Interaction
+{
+    public UnityEvent interactionEvent;
+
+    public override void Interact()
+    {
+        interactionEvent?.Invoke();
+    }
+}

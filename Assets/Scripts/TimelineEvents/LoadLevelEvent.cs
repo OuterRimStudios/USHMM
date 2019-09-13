7d04859e019129a677231eaa7f3e7acb0921e5bf
Sam Gebhard <33882552+sgebhard6@users.noreply.github.com>
2019-09-13 14:06:25 -0500

    added steamvr_loadlevel

---
+++ Assets/Scripts/TimelineEvents/LoadLevelEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class LoadLevelEvent : OuterRimStudios.Event
+{
+    public string sceneName;
+
+    public override void StartEvent()
+    {
+        print("should load: " + sceneName);
+        SteamVR_LoadLevel.Begin(sceneName);
+    }
+}

5eeaecdf9a47ef11478e0ac7b41e6deaf67d2b3f
hectorborges <hectorborges94@gmail.com>
2019-09-13 15:30:39 -0500

    added agrab manager to stop the player from being able to grab multiple objects

---
+++ Assets/GrabManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabManager : MonoBehaviour
+{
+    public static GrabManager Instance;
+
+    public bool LeftHandBusy { get; private set; }
+    public bool RightHandBusy { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Grab(Handedness handedness)
+    {
+        if (handedness == Handedness.Left)
+            LeftHandBusy = true;
+        else
+            RightHandBusy = true;
+    }
+
+    public void Drop(Handedness handedness)
+    {
+        if (handedness == Handedness.Left)
+            LeftHandBusy = false;
+        else
+            RightHandBusy = false;
+    }
+
+    public bool IsBusy(Handedness handedness)
+    {
+        return handedness == Handedness.Left ? LeftHandBusy : RightHandBusy;
+    }
+}

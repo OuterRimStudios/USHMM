f4cc86c6546d8ab92e62d3890db375a31f08a37e
hectorborges <hectorborges94@gmail.com>
2019-09-17 09:36:13 -0500

    added lightswitch sound, added pick up sound

--- Assets/Scripts/Interact/Interactions/AudioInteraction.cs
+++ Assets/Scripts/Interact/Interactions/AudioInteraction.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class AudioInteraction : Interaction
 {
-    public AudioSource audioSource;
+    public AudioClip[] audioClips;
+    public bool isRepeatable;
+
+    AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     public override void Interact()
     {
+        if (!isRepeatable) return;
+
         base.Interact();
-        audioSource.Play();
+        audioSource.PlayOneShot(audioClips[Random.Range(0, audioClips.Length)]);
     }
 
     public override void StopInteraction()
---
+++ Assets/ToggleAudioInteraction.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleAudioInteraction : Interaction
+{
+    public AudioClip[] initialSounds;
+    public AudioClip[] secondarySounds;
+
+    public AudioSource[] initialSources;
+    public AudioSource[] secondarySources;
+
+    AudioSource source;
+
+    bool toggle;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public override void Interact()
+    {
+        if (!toggle)
+        {
+            foreach(AudioSource source in initialSources)
+            {
+                source.clip = initialSounds[Random.Range(0, initialSounds.Length)];
+                source.Play();
+            }
+
+            foreach(AudioSource source in secondarySources)
+                source.Stop();
+        }
+        else
+        {
+            foreach(AudioSource source in secondarySources)
+            {
+                source.clip = secondarySounds[Random.Range(0, secondarySounds.Length)];
+                source.Play();
+            }
+
+            foreach (AudioSource source in initialSources)
+                source.Stop();
+        }
+    }
+}

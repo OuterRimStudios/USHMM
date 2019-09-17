a328f5b8077ca96fae3dc65763319ff997423a74
hectorborges <hectorborges94@gmail.com>
2019-09-17 10:36:41 -0500

    added set down code

---
+++ Assets/Scripts/Interact/Interactions/TriggerAudioInteraction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAudioInteraction : Interaction
+{
+    public AudioClip[] clips;
+
+    AudioSource source;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!other.tag.Equals("Controller"))
+            source.PlayOneShot(clips[Random.Range(0, clips.Length)]);
+    }
+}

e356a7f916f49300bfb04c69cb320931bbca6ca0
Sam Gebhard <33882552+sgebhard6@users.noreply.github.com>
2019-09-18 10:08:42 -0400

    removed restart event from all scenes except the last

---
+++ Assets/Scripts/RestartEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using Valve.VR;
+
+public class RestartEvent : MonoBehaviour
+{
+    public SteamVR_LoadLevel levelLoader;
+    bool isDone;
+    void Update()
+    {
+        if (!isDone && XRDevice.userPresence == UserPresenceState.NotPresent)
+            isDone = true;
+
+        if (isDone && XRDevice.userPresence == UserPresenceState.Present)
+            levelLoader.Trigger(0);
+    }
+}

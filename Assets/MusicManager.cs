f8c9ceed408e37b97faf453bd534932fb30af25a
hectorborges <hectorborges94@gmail.com>
2019-09-20 11:31:30 -0500

    adjusted hand poses, updated audio interactions, added some voice over

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager Instance;
+
+    public AudioSource musicSource;
+
+    [Range(0, 1)]
+    public float originalVolume;
+    [Range(0, 1)]
+    public float loweredVolume;
+
+    public float musicTransitionSpeed = .5f;
+    public float sourceTransitionSpeed = 1;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void AudioEventStarted(AudioSource source)
+    {
+        StartCoroutine(Transition(loweredVolume, source, 1));
+    }
+
+    public void AudioEventEnded(AudioSource source)
+    {
+        StartCoroutine(Transition(originalVolume, source, 0));
+    }
+
+    IEnumerator Transition(float musicVolume, AudioSource source, float sourceVolume)
+    {
+        yield return new WaitUntil(() => Transitioned(musicVolume, source, sourceVolume));
+    }
+
+    bool Transitioned(float musicVolume, AudioSource source, float sourceVolume)
+    {
+        source.volume = Mathf.MoveTowards(source.volume, sourceVolume, musicTransitionSpeed * Time.deltaTime);
+        musicSource.volume = Mathf.MoveTowards(musicSource.volume, musicVolume, musicTransitionSpeed * Time.deltaTime);   
+        return musicSource.volume == musicVolume;
+    }
+
+}
---
+++ Assets/Scripts/ResetInteractable.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OuterRimStudios.Utilities;
+public class ResetInteractable : MonoBehaviour
+{
+    public float resetTime = 2.5f;
+    float time;
+
+    Vector3 startingPosition;
+    Quaternion startingRotation;
+    Rigidbody rb;
+
+    bool wasGrabbed;
+    bool isGrabbed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        time = resetTime;                               
+        startingPosition = transform.position;          
+        startingRotation = transform.rotation;          
+    }
+
+    private void Update()
+    {
+        if(wasGrabbed && !isGrabbed)
+        {
+            MathUtilities.Timer(ref time);
+
+            if (time <= 0)
+                Reset();
+        }
+    }
+
+    public void GrabObject()
+    {
+        if (!wasGrabbed) wasGrabbed = true;
+        isGrabbed = true;
+    }
+
+    public void LetGo()
+    {
+        isGrabbed = false;
+    }
+
+    private void Reset()
+    {
+        rb.velocity = Vector3.zero;
+        transform.position = startingPosition;  //Set the position of this object back to it's starting position
+        transform.rotation = startingRotation;  //Set the rotation of this object back to it's starting rotation
+        time = resetTime;                       //Reset the timer for the next time the object is grabbed
+    }
+}

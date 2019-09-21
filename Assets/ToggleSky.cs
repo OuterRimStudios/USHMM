39e0e9d7f53123da24a4925dd7c5e39ed94c20da
hectorborges <hectorborges94@gmail.com>
2019-09-21 11:25:09 -0500

    added new transitions

---
+++ Assets/ToggleSky.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleSky : MonoBehaviour
+{
+    public Camera cam;
+    public void Toggle()
+    {
+        cam.clearFlags = CameraClearFlags.Skybox;
+    }
+}

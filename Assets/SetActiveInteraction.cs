7a64037066ba6710a4bcd2bc17e73a71f7dedfde
hectorborges <hectorborges94@gmail.com>
2019-09-21 13:01:10 -0500

    play test changes

---
+++ Assets/SetActiveInteraction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActiveInteraction : Interaction
+{
+    public GameObject[] objectsToActivate;
+
+    public override void Interact()
+    {
+        foreach (GameObject go in objectsToActivate)
+            go.SetActive(!go.activeSelf);
+    }
+
+}

4cd0b0461a211873d6aeee1a4417641b62e2a5e5
hectorborges <hectorborges94@gmail.com>
2019-09-23 11:56:20 -0500

    added new transition images

---
+++ Assets/Scripts/LoadSceneInteraction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class LoadSceneInteraction : Interaction
+{
+    public SteamVR_LoadLevel sceneLoader;
+    public int sceneIndex;
+    public override void Interact()
+    {
+        sceneLoader.Trigger(sceneIndex);
+    }
+}

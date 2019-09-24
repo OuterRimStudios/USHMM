6a5fbe87f9ce50c987d5138e9879aeb154701ae2
hectorborges <hectorborges94@gmail.com>
2019-09-24 09:20:29 -0500

    added one time interaction

---
+++ Assets/OneTimeInteraction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneTimeInteraction : Interaction
+{
+    public AudioInteraction interaction;
+    public override void Interact()
+    {
+        if (!interaction.HasPlayed)
+            interaction.Interact();
+    }
+}

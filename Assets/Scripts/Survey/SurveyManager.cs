2efc3fd593f28c876aa728a76127bf48b81ef2e5
Sam <33882552+sgebhard6@users.noreply.github.com>
2019-09-24 09:07:08 -0500

    updated survey manager to split analytics data

--- Assets/Scripts/Survey/SurveyManager.cs
+++ Assets/Scripts/Survey/SurveyManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Analytics;
 using OuterRimStudios.Utilities;
+using CsvHelper.Configuration.Attributes;
 
 public class SurveyManager : MonoBehaviour
 {
@@ -52,14 +53,29 @@ public class SurveyManager : MonoBehaviour
 
     public void Submit()
     {
-        var data = new List<object>();
+        var data = new List<SurveyResults>
+        {
+            new SurveyResults{ Question1 = questions[0].answer, Question2 = questions[1].answer, Question3 = questions[2].answer, Question4 = questions[3].answer }
+        };
         //Add all of the answers to a list that will be sent to the analytics
-        for (int i = 0; i < questions.Count - 1; i++)
-            data.Add(new { Answer = questions[i].answer }); //Analytics.CustomEvent("SurveyResults", new Dictionary<string, object> { { "Question " + (i + 1), questions[i].answer } });
+        //for (int i = 0; i < questions.Count - 1; i++)
+            //data.Add(new { Answer = questions[i].answer }); //Analytics.CustomEvent("SurveyResults", new Dictionary<string, object> { { "Question " + (i + 1), questions[i].answer } });
 
         AnalyticsUtilities.Event("SurveyResults", data);
 
         Debug.Log("Survey Submitted");
         //Send to analytics
     }
+}
+
+public class SurveyResults
+{
+    [Name("This experience made me feel curious.")]
+    public string Question1 { get; set; }
+    [Name("This experience made me feel sad.")]
+    public string Question2 { get; set; }
+    [Name("This experience made me feel compassionate.")]
+    public string Question3 { get; set; }
+    [Name("This experience made me feel distressed.")]
+    public string Question4 { get; set; }
 }

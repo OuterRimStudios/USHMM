9e602e0c19a62a1d2bc42e1dcdf73d792a87e01f
Sam <33882552+sgebhard6@users.noreply.github.com>
2019-09-25 07:01:53 -0500

    analytics now save to the documents folder

--- Assets/Scripts/Utilities/AnalyticsUtilities.cs
+++ Assets/Scripts/Utilities/AnalyticsUtilities.cs
@@ -14,17 +14,18 @@ namespace OuterRimStudios.Utilities
         /// <param name="data">The data that you want written to the csv file."</param>
         public static void Event<T>(string eventName, List<T> data)
         {
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             //This bool controls whether or not the Write operation adds the headers for the data. If the file already exists, then it should already have the headers and this bool should remain false.
             bool createHeaders = false;
             //Adding the date to the filename so the analytics are separated based on date.
             string date = DateTime.Today.ToString("d").Replace('/', '-');
-            if (!Directory.Exists(Application.dataPath + "\\Analytics\\"))
-                Directory.CreateDirectory(Application.dataPath + "\\Analytics\\");
-            if (!File.Exists(Application.dataPath + "\\Analytics\\" + eventName + "Analytics_" + date + ".csv"))
+            if (!Directory.Exists(documentsPath + "\\Analytics\\"))
+                Directory.CreateDirectory(documentsPath + "\\Analytics\\");
+            if (!File.Exists(documentsPath + "\\Analytics\\" + eventName + "Analytics_" + date + ".csv"))
                 createHeaders = true;
 
             //This section is writing the data to the csv file
-            using (var writer = new StreamWriter(Application.dataPath + "\\Analytics\\" + eventName + "Analytics_" + date + ".csv", append: true))
+            using (var writer = new StreamWriter(documentsPath + "\\Analytics\\" + eventName + "Analytics_" + date + ".csv", append: true))
             using (var csv = new CsvWriter(writer))
             {
                 csv.Configuration.HasHeaderRecord = createHeaders;

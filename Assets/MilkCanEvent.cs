56a98b5537166c8e0f0fbb3efd836742d0f5d1ae
hectorborges <hectorborges94@gmail.com>
2019-09-25 10:19:16 -0500

    milk can triggers after time if not triggered

---
+++ Assets/MilkCanEvent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MilkCanEvent : Interaction
+{
+    public AudioInteraction sound;
+    public Animator animator;
+    public override void Interact()
+    {
+        if(!animator.GetBool("Open"))
+        {
+            animator.SetBool("Open", true);
+            sound.Interact();
+        }
+    }
+}

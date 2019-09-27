525c133ee6c38e408bcc43a443eb1650a00701e2
hectorborges <hectorborges94@gmail.com>
2019-09-27 09:05:04 -0500

    milk can's height adjusts to user's height

---
+++ Assets/SetPosition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPosition : MonoBehaviour
+{
+    public float offset;
+
+    private void Start()
+    {
+        float playerHeight = Camera.main.transform.position.y;
+        transform.position = new Vector3(transform.position.x, playerHeight - offset, transform.position.z);
+    }
+}
